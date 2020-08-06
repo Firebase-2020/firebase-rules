3f9dcae729efde2a284d38c16289a53fa1f09812
fotis tsakiris <footios76@gmail.com>
2020-08-06 14:42:33 +0300

    added name input and buttons

--- src/App.tsx
+++ src/App.tsx
@@ -7,12 +7,20 @@ export interface AppProps {
 
 
 const App: React.SFC<AppProps> = () => {
+  const [name, setName] = useState<string>();
   const [email, setEmail] = useState<string>();
   const [password, setPassword] = useState<string>();
+  const [login, setLogin] = useState<boolean>(false);
+
+  const handleName = (event: React.SyntheticEvent<EventTarget>) => {
+    if (!!password) {
+      setName((event.target as HTMLInputElement).value)
+    }
+  }
 
   const handleEmail = (event: React.SyntheticEvent<EventTarget>) => {
-    console.log((event.target as HTMLInputElement).value);
-    
+    // console.log((event.target as HTMLInputElement).value);
+
     if (!!email) {
       setEmail((event.target as HTMLInputElement).value)
     }
@@ -23,15 +31,59 @@ const App: React.SFC<AppProps> = () => {
     }
   }
 
-  handleSubmit = (event) => {
-   
+  const handleSubmit = (event: React.SyntheticEvent<EventTarget>) => {
+    event.preventDefault();
+    // Check if form is valid
+    if (true) {
+      // // Reset errors and set loading to true
+      // firebase
+      //   .auth()
+      //   .signInWithEmailAndPassword(email, password)
+      //   .then(singedInUser => {
+      //     console.log("singedInUser", singedInUser);
+      //     // set loading to false
+      //   })
+      //   .catch(err => {
+      //     console.error(err);
+      //     // set errors
+      //     // set loading to false
+      //   });
+    }
   };
 
   return (
     <div className='containter' >
+      <h2>{login ? 'Login' : 'Sign up'} </h2>
+      <button
+        type="button"
+        className="login-button"
+        onClick={() => setLogin(prev => !prev)}
+      >
+        {login ? "need to create an account?" : "already have an account"}
+      </button>
       <form onClick={handleSubmit}>
-        <input onChange={handleEmail} value={email} placeholder='email' className='email' id='email' style={{ marginRight: 10 }} />
-        <input onChange={handlePassword} value={password} placeholder='password' className='password' />
+        {!login && (
+          <input
+            value={name}
+            onChange={handleName}
+            type="text"
+            placeholder="Your name"
+            autoComplete="off"
+            style={{ marginRight: 10 }}
+          />
+        )}
+        <input onChange={handleEmail} type="text" value={email} placeholder='Your email' className='email' id='email' autoComplete="off" style={{ marginRight: 10 }} />
+        <input onChange={handlePassword} type="text" value={password} placeholder='Your password' className='password' autoComplete="off" />
+        <div className="actions">
+          <button
+            type="submit"
+            className="submit_button"
+            style={{ background: "orange" }}
+          >
+            Submit
+          </button>
+
+        </div>
       </form>
     </div>);
 }
